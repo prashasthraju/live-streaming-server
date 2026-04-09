e219b98539640f3ba83241e30825cafcb22668b0
OMKAR_GAJULA <147458454+OMKAROG@users.noreply.github.com>
2026-04-09 21:26:18 +0530

    Add files via upload

---
+++ backend/include/HTTPRequest.h
@@ -0,0 +1,29 @@
+#ifndef HTTPRequest_h
+#define HTTPRequest_h
+
+#include "DataStructures/Dictionary/Dictionary.h"
+
+enum HTTPmethods
+{
+    GET,
+    POST,
+    PUT,
+    HEAD,
+    PATCH,
+    DELETE,
+    CONNECT,
+    OPTIONS,
+    TRACE
+};
+
+struct HTTPReqest
+{
+    int Method;
+    char *URI;
+    float HTTPVersion;
+    struct Dictionary header_fields;
+}
+
+struct HTTPRequest http_request_constructor(char *request_string);
+
+#endif
